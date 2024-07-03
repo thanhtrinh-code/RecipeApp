9e5774c9f98b9121e0c2d06e9e62c429c05158f6
Thanh Duy Trinh <thanhtrinh501@Thanhs-MacBook-Pro-2.local>
2024-07-02 22:07:35 -0700

    app completed

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import RecipeCard from '../components/RecipeCard';
+import {getRandomColor} from '../lib/utils';
 const FavoritesPage = () => {
-  const fav = false;
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
   return (
     <div className='bg-[#faf9fb] flex-1 p-10 min-h-screen'>
       <div className='max-w-screen-lg mx-auto'>
           <p className='font-bold text-3xl md:text-5xl my-4'>
             My Favorites</p>
-            {!fav && (
-              <div className='h-[80vh] flex flex-col items-center gap-4'>
-                  <img src="/404.svg" className='h-3/4' alt="404 svbg" />
-              </div>
-            )}
-            {fav && (
+              {favorites.length === 0 && (
+                <div className='h-[80vh] flex flex-col items-center gap-4'>
+                  <img src="/404.svg" className='h-3/4' alt="404 avg" />
+                </div>
+              )}
               <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-                <RecipeCard/>
+                {favorites.map((recipe) => (
+                  <RecipeCard key={recipe.label} recipe={recipe} {...getRandomColor()}/>
+                ))
+                }
               </div>
-            )}
       </div>
     </div>
   )
